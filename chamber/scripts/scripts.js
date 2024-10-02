0063a2eff76aeb6c5d92d596ef7e7ee8e701dba1
stanleynephi <nul23001@byui.edu>
2024-10-02 10:44:45 +0000

    update on json file

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -199,7 +199,6 @@ copyRight()
 //work on adding business to the main section of the directory
 
 
-//step one make a call to the member api in the data folder
 const memberApiUrl = "https://stanleynephi.github.io/wdd231/chamber/data/members.json";
 
 async function MembersApiData() {
@@ -210,12 +209,21 @@ async function MembersApiData() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+            return data; // Return the fetched data for further use
         } else {
             console.log(`HTTP error: ${response.status}`);
+            const main = document.querySelector(".businessess");
+            if (main) {
+                main.innerHTML = `Sorry, an error occurred: ${response.status} ${response.statusText}`;
+            }
         }
         
     } catch (error) {
         console.log(`This error was found in your code: ${error}`);
+        const main = document.querySelector(".businessess");
+        if (main) {
+            main.innerHTML = "An unexpected error occurred. Please try again later.";
+        }
     }
 }
 
